52c19b0448c2a9f45db1c5f38cce9e1f4001ce3b
Erick Malta <erickmalta100@gmail.com>
2020-08-28 22:33:10 -0300

    feat: :ok_hand: add logic to create method

--- src/modules/users/controllers/UserController.ts
+++ src/modules/users/controllers/UserController.ts
@@ -2,7 +2,16 @@ import { Request, Response } from 'express';
 
 class UserController {
   async create(request: Request, response: Response) {
-    return response.send('Controller do Usuário');
+    const { name, email, password, password_confirm } = request.body;
+
+    if (!name) return response.json({ message: 'name is required'});
+
+    if (!email) return response.json({ message: 'email is required'});
+    if (!password) return response.json({ message: 'password is required'});
+    if (!password_confirm) return response.json({ message: 'password_confirm is required'});
+    if (password !== password_confirm) return response.json({ message: "passwords don't match"});
+    
+    return response.json({ name, email, password, password_confirm });
   }
 }
 
