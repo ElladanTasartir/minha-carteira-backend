32586110906a7bc450ef0ad961f2e468ad3b1c48
Erick Malta <erickmalta100@gmail.com>
2020-08-29 23:34:22 -0300

    style: :nail_care: removed spacing between validations

--- src/modules/users/controllers/UserController.ts
+++ src/modules/users/controllers/UserController.ts
@@ -5,7 +5,6 @@ class UserController {
     const { name, email, password, password_confirm } = request.body;
 
     if (!name) return response.json({ message: 'name is required'});
-
     if (!email) return response.json({ message: 'email is required'});
     if (!password) return response.json({ message: 'password is required'});
     if (!password_confirm) return response.json({ message: 'password_confirm is required'});
