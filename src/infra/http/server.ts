975840e5d83bd417b20d854dc7321f20185cc0d6
Erick Malta <erickmalta100@gmail.com>
2020-09-04 22:35:42 -0300

    feat: :ok_hand: add global error handler

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,14 +1,26 @@
 import 'reflect-metadata';
-import express, { json } from 'express';
+import express, { json, NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import '../database';
 import routes from './routes/index';
+import AppError from '../../shared/errors/AppError';
 
 const server = express();
 
 server.use(json());
 
 server.use(routes);
+
+server.use(
+  (err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({ message: 'Internal Server Error' });
+  },
+);
 
 server.listen(3333, () => {
   // eslint-disable-next-line
