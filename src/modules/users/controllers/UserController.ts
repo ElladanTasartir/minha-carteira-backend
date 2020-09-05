31ab7bcf247bb411b9cb4e83f6e81a5700f60ba9
Erick Malta <erickmalta100@gmail.com>
2020-09-04 22:12:28 -0300

    feat: :ok_hand: add SignUpService and validation

--- src/modules/users/controllers/UserController.ts
+++ src/modules/users/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import SignUpService from '../services/SignUpService';
 
 class UserController {
   async create(request: Request, response: Response) {
@@ -12,7 +13,21 @@ class UserController {
     if (password !== password_confirm)
       return response.json({ message: "passwords don't match" });
 
-    return response.json({ name, email, password, password_confirm });
+    const signUp = new SignUpService();
+
+    try {
+      const userResponse = await signUp.execute({
+        name,
+        email,
+        password,
+      });
+
+      delete userResponse.password;
+
+      return response.json(userResponse);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
