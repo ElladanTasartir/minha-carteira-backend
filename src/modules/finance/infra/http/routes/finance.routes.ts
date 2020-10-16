ed7de7edd419059695c2686881129073c15b3863
Erick Malta <erickmalta100@gmail.com>
2020-10-16 20:37:57 -0300

    feat: :ok_hand: add finance routes

---
+++ src/modules/finance/infra/http/routes/finance.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import EntryController from '../controllers/EntryController';
+
+const financeRouter = Router();
+
+const entryController = new EntryController();
+
+financeRouter.use(ensureAuthenticated);
+financeRouter.post('/entry', entryController.create);
+
+export default financeRouter;
