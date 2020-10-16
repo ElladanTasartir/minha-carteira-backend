ab6d76400ab2e96b5f6539e81b8b47f048b42f02
Erick Malta <erickmalta100@gmail.com>
2020-10-16 20:38:21 -0300

    feat: :ok_hand: add finance routes to server routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import userRoutes from '@modules/users/infra/http/routes/user.routes';
 import authRoutes from '@modules/users/infra/http/routes/auth.routes';
+import financeRoutes from '@modules/finance/infra/http/routes/finance.routes';
 
 const routes = Router();
 
 routes.use('/users', userRoutes);
 routes.use('/auth', authRoutes);
+routes.use('/finance', financeRoutes);
 
 export default routes;
