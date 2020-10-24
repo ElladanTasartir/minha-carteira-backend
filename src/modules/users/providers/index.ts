40a8bef46c5a34c85b6f49520b32b255170ba6e8
Erick Malta <erickmalta100@gmail.com>
2020-10-23 22:13:45 -0300

    feat: :ok_hand: inject TokenProvider

--- src/modules/users/providers/index.ts
+++ src/modules/users/providers/index.ts
@@ -3,4 +3,9 @@ import { container } from 'tsyringe';
 import IHashProvider from './HashProvider/models/IHashProvider';
 import BcryptHashProvider from './HashProvider/implementations/BcryptHashProvider';
 
+import ITokenProvider from './TokenProvider/models/ITokenProvider';
+import JWTProvider from './TokenProvider/implementations/JWTProvider';
+
 container.registerSingleton<IHashProvider>('HashProvider', BcryptHashProvider);
+
+container.registerSingleton<ITokenProvider>('TokenProvider', JWTProvider);
