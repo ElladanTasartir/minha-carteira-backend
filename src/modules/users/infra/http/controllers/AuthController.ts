f9025d2552d77dc65d746866f8ba369fc04b5bd5
Erick Malta <erickmalta100@gmail.com>
2020-10-30 22:34:12 -0300

    feat: :ok_hand: remove AppError and validations in controllers

--- src/modules/users/infra/http/controllers/AuthController.ts
+++ src/modules/users/infra/http/controllers/AuthController.ts
@@ -3,14 +3,10 @@ import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
 
 import SignInService from '@modules/users/services/SignInService';
-import AppError from '@shared/errors/AppError';
 
 class AuthController {
   async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
-
-    if (!email) throw new AppError('email is required', 400);
-    if (!password) throw new AppError('password is required', 400);
 
     const signUp = container.resolve(SignInService);
 
--- src/modules/users/infra/http/controllers/UserController.ts
+++ src/modules/users/infra/http/controllers/UserController.ts
@@ -3,19 +3,10 @@ import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
 
 import SignUpService from '@modules/users/services/SignUpService';
-import AppError from '@shared/errors/AppError';
 
 class UserController {
   async create(request: Request, response: Response): Promise<Response> {
-    const { name, email, password, password_confirm } = request.body;
-
-    if (!name) throw new AppError('name is required', 400);
-    if (!email) throw new AppError('email is required', 400);
-    if (!password) throw new AppError('password is required', 400);
-    if (!password_confirm)
-      throw new AppError('password_confirm is required', 400);
-    if (password !== password_confirm)
-      throw new AppError("passwords don't match", 400);
+    const { name, email, password } = request.body;
 
     const signUp = container.resolve(SignUpService);
 
