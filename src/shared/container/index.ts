7ba7761748cf962dd183168e79e613bccdeaa8d8
Erick Malta <erickmalta100@gmail.com>
2020-10-30 22:47:17 -0300

    feat: :ok_hand: add FinanceRepository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,4 +5,12 @@ import '@modules/users/providers';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 
+import IFinanceRepository from '@modules/finance/repositories/IFinanceRepository';
+import FinanceRepository from '@modules/finance/infra/typeorm/repositories/FinanceRepository';
+
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<IFinanceRepository>(
+  'FinanceRepository',
+  FinanceRepository,
+);
