afe36c1bc52258142865170c3699cc55b24c8b28
Erick Malta <erickmalta100@gmail.com>
2020-10-30 22:23:19 -0300

    feat: :ok_hand: rename controller and add service call

--- src/modules/finance/infra/http/controllers/EntryController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export default class EntryController {
-  public async create(request: Request, response: Response): Promise<Response> {
-    return response.send('teste');
-  }
-}
---
+++ src/modules/finance/infra/http/controllers/MovementController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import MovementRegister from '@modules/finance/services/MovementRegister';
+
+export default class MovementController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { title, type, date, frequency, amount, description } = request.body;
+
+    const movementRegister = container.resolve(MovementRegister);
+
+    const movement = await movementRegister.execute({
+      title,
+      type,
+      date,
+      frequency,
+      amount,
+      description,
+    });
+
+    return response.status(201).json(movement);
+  }
+}
