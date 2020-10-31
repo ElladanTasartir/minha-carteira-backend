e0bd5a0b65868cc84ce59b14b96d260a81b1d99e
Erick Malta <erickmalta100@gmail.com>
2020-10-30 22:32:34 -0300

    feat: :ok_hand: add FinanceRepository

---
+++ src/modules/finance/infra/typeorm/repositories/FinanceRepository.ts
@@ -0,0 +1,23 @@
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import IFinanceRepository from '@modules/finance/repositories/IFinanceRepository';
+import ICreateMovementDTO from '@modules/finance/dtos/ICreateMovementDTO';
+import Movement from '../entities/Movement';
+
+class FinanceRepository implements IFinanceRepository {
+  private ormRepository: MongoRepository<Movement>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Movement);
+  }
+
+  public async create(data: ICreateMovementDTO): Promise<Movement> {
+    const movement = this.ormRepository.create(data);
+
+    await this.ormRepository.save(movement);
+
+    return movement;
+  }
+}
+
+export default FinanceRepository;
