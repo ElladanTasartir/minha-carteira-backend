729542732bb7e290086b690ba26e78b26fa99c41
Erick Malta <erickmalta100@gmail.com>
2020-11-06 23:06:29 -0300

    feat: :ok_hand: add new methods to model and fakes

--- src/modules/finance/repositories/IFinanceRepository.ts
+++ src/modules/finance/repositories/IFinanceRepository.ts
@@ -1,7 +1,13 @@
 import ICreateMovementDTO from '../dtos/ICreateMovementDTO';
+import IFindMovementsByUser from '../dtos/IFindMovementsByUser';
 
 import Movement from '../infra/typeorm/schemas/Movement';
 
 export default interface IFinanceRepository {
   create(data: ICreateMovementDTO): Promise<Movement>;
+  findMovementsByUser(
+    data: IFindMovementsByUser,
+  ): Promise<Movement[] | undefined>;
+  findMovementById(id: string): Promise<Movement | undefined>;
+  deleteMovement(id: string): Promise<void>;
 }
--- src/modules/finance/repositories/fakes/FakeFinanceRepository.ts
+++ src/modules/finance/repositories/fakes/FakeFinanceRepository.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'mongodb';
 
 import Movement from '@modules/finance/infra/typeorm/schemas/Movement';
 import ICreateMovementDTO from '@modules/finance/dtos/ICreateMovementDTO';
+import IFindMovementByUser from '@modules/finance/dtos/IFindMovementsByUser';
 import IFinanceRepository from '../IFinanceRepository';
 
 export default class FakeFinanceRepository implements IFinanceRepository {
@@ -15,5 +16,38 @@ export default class FakeFinanceRepository implements IFinanceRepository {
     this.movements.push(movement);
 
     return movement;
+  }
+
+  public async findMovementsByUser({
+    user_id,
+    frequency,
+    type,
+  }: IFindMovementByUser): Promise<Movement[] | undefined> {
+    const movements = this.movements.filter(
+      movement =>
+        movement.user_id === user_id &&
+        movement.frequency === frequency &&
+        movement.type === type,
+    );
+
+    return movements;
+  }
+
+  public async findMovementById(id: string): Promise<Movement | undefined> {
+    const movementObjectId = new ObjectId(id);
+
+    const findMovement = this.movements.find(
+      movement => String(movement.id) === String(movementObjectId),
+    );
+
+    return findMovement;
+  }
+
+  public async deleteMovement(id: string): Promise<void> {
+    const deletedMovementIndex = this.movements.findIndex(
+      movement => String(movement.id) === String(id),
+    );
+
+    this.movements.splice(deletedMovementIndex, 1);
   }
 }
