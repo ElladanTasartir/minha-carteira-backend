2fba6235afb15b6ade44f0c504034322d9662979
Erick Malta <erickmalta100@gmail.com>
2020-11-06 23:15:25 -0300

    feat: :ok_hand: add DeleteMovement service

---
+++ src/modules/finance/services/DeleteMovement.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IFinanceRepository from '../repositories/IFinanceRepository';
+
+interface IRequest {
+  id: string;
+  user_id: string;
+}
+
+@injectable()
+export default class ShowMovement {
+  constructor(
+    @inject('FinanceRepository')
+    private financeRepository: IFinanceRepository,
+  ) {}
+
+  public async execute({ id, user_id }: IRequest): Promise<void> {
+    const movement = await this.financeRepository.findMovementById(id);
+
+    if (movement?.user_id !== user_id) {
+      throw new AppError('You cannot delete this movement', 401);
+    }
+
+    await this.financeRepository.deleteMovement(id);
+  }
+}
